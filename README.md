075b9cb6109c378ae1b30cdcee86d614fecfbde3
Angela Zhang <angelazhang136@gmail.com>
2024-08-14 06:21:53 +0000

    done

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # -506-practice-workflow-
 This is a primer for 504/507 Fall semester classes to practice a basic development workflow
+
+Explanation of code:
+The code imports the pandas library as pd and numpy as np.
+A number variable (numb_variable) is assigned 10
+A string variable (string_variable) is assigned the string "hello world".
+A dictionary variable (my_dict) is created with three key-value pairs, including a list and a nested dictionary.
+The printed output should be 10, hello world and all the dictionary pares
+
+For the function, I gave it the function name analyze_bmi. The two variables to calculate bmi are weight and height. I gave it some parameters and made the height to be 175m and the weight to be 70kg. The return value would be categorized into underweight, normal, or overweight. 
--- intro_practice.py
+++ intro_practice.py
@@ -1,2 +1,40 @@
 import pandas as pd
 import numpy as np
+
+num_variable = 10
+string_variable = "hello world"
+my_dict = {
+    'age' : '22',
+    'firstName' : 'Angela',
+    'lastName' : 'Zhang',
+    'list' : [1, 2, 3], 
+    'nested_dictionary' : {
+        'sex' : 'female',
+        'race' : 'asian',
+
+    }
+}
+
+print("number variables:", num_variable)
+print("string variables:", string_variable)
+print("dictionaries:", my_dict)
+
+
+def analyze_bmi(height,weight):
+
+#How to calculate bmi
+
+    bmi = weight / (height**2)
+    if bmi < 18.5:
+        return f"underweight"
+    elif 18.5 <= bmi < 24.9:
+        return f"normal"
+    else:
+        return f"overweight"
+    
+height_input = 1.78  # height in meters
+weight_input = 73    # weight in kilograms
+    
+bmi_category = analyze_bmi(height_input, weight_input)
+print("BMI Category:", bmi_category)
+
